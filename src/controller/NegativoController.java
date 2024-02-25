4ff4f163b63065202924f97fee374ceb742982aa
LucasSantosBatista <142185716+LucasSantosBatista@users.noreply.github.com>
2024-02-25 16:46:21 -0300

    Commit

--- src/controller/NegativoController.java
+++ src/controller/NegativoController.java
@@ -7,13 +7,16 @@ public class NegativoController {
 	}
 	
 	public int contNegativo(int[] vet, int tamanho, int cont) {
+		//Condição de parada: contagem é zero, percorrendo todo vetor
 		if (tamanho == 0) {
 			return cont;
 		} else {
+			//Verifica se valor na ultima posição é negativo. Se sim, adiciona a contagem
 			if (vet[tamanho] < 0) {
 				cont++;
 			} 
 		}
+		//Recursiva chamando função com contagem de posição - 1
 		return contNegativo (vet, tamanho-1, cont);
 	}
 }
